0e2ab05e4c5f0431ea4e880e843e91da2caf4e58
saitota <1152469+saitota@users.noreply.github.com>
2018-03-10 19:53:30 +0900

    Split ymf file for each environment

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ print('Loading function... ')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
+def event_to_json(event):
+    if 'body' in event:
+        body = json.loads(event.get('body'))
+        return body
+    elif 'token' in event:
+        body = event
+        return body
+    else:
+        logger.error('unexpected event format')
+        exit
+
+
 def handler(event, context):
     #getenv
     OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
@@ -14,16 +27,10 @@ def handler(event, context):
     HOOK_REACTION = os.environ['HOOK_REACTION']
     ADD_REACTIONS = os.environ['ADD_REACTIONS']
 
-    #受信したjsonをLogsに出力
-    logging.info(json.dumps(event))
-    # json処理
-    if 'body' in event:
-        body = json.loads(event.get('body'))
-    elif 'token' in event:
-        body = event
-    else:
-        logger.error('unexpected event format')
-        return 'error'
+    # Output the received event to the log
+    # logging.info(json.dumps(event))
+    body = event_to_json(event)
+
     #url_verificationイベントに返す
     if 'challenge' in body:
         challenge = body.get('challenge')
@@ -39,7 +46,7 @@ def handler(event, context):
         # add か remove か決める
         if body.get('event').get('type') == 'reaction_added':
             url = 'https://slack.com/api/reactions.add'
-        elif  body.get('event').get('type') == 'reaction_removed':
+        elif body.get('event').get('type') == 'reaction_removed':
             url = 'https://slack.com/api/reactions.remove'
 
         headers = {
@@ -49,15 +56,19 @@ def handler(event, context):
         # reaction定義
         emoji = ADD_REACTIONS.split(',')
         for e in emoji:
-            print( e)
+            print(e)
             data = {
                 'token': OAUTH_TOKEN,
                 'channel': body.get('event').get('item').get('channel'),
                 'timestamp': body.get('event').get('item').get('ts'),
                 'name': e
             }
-            # POST処理　
-            req = urllib.request.Request(url, data=json.dumps(data).encode('utf-8'), method='POST', headers=headers)
+            # POST処理
+            req = urllib.request.Request(
+                url,
+                data=json.dumps(data).encode('utf-8'),
+                method='POST',
+                headers=headers)
             res = urllib.request.urlopen(req)
             logger.info('post result: %s', res.msg)
         return {'statusCode': 200, 'body': 'ok'}
