0a0f7f0699961e009a11fa94d28eb44a413dae17
saitota <1152469+saitota@users.noreply.github.com>
2018-03-12 22:30:12 +0900

    changed to ensure ordering

--- main.py
+++ main.py
@@ -19,54 +19,70 @@ def event_to_json(event):
         logger.error('unexpected event format')
         exit
 
+
 class ChallangeJson(object):
-     def data(self,key):
-          return {
+    def data(self, key):
+        return {
             'isBase64Encoded': 'true',
             'statusCode': 200,
             'headers': {},
             'body': key
         }
 
+
 def handler(event, context):
     #getenv
     OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
     BOT_TOKEN = os.environ['BOT_TOKEN']
-    HOOK_REACTION = os.environ['HOOK_REACTION']
-    ADD_REACTIONS = os.environ['ADD_REACTIONS']
-
+    #HOOK_REACTION = os.environ['HOOK_REACTION']
+    HOOK_REACTIONS = os.environ['HOOK_REACTIONS']
     # Output the received event to the log
-    # logging.info(json.dumps(event))
+    logging.info(json.dumps(event))
     body = event_to_json(event)
     # return if it was challange-event
     if 'challenge' in body:
         challenge_key = body.get('challenge')
         logging.info('return challenge key %s:', challenge_key)
         return ChallangeJson().data(challenge_key)
-
-    # 特定のリアクションだったら
-    if body.get('event').get('reaction') == HOOK_REACTION:
-        # add か remove か決める
-        if body.get('event').get('type') == 'reaction_added':
-            url = 'https://slack.com/api/reactions.add'
-        elif body.get('event').get('type') == 'reaction_removed':
-            url = 'https://slack.com/api/reactions.remove'
+    # define reaction
+    emojis = HOOK_REACTIONS.split(',')
+    if body.get('event').get('reaction') in emojis:
+        logger.info('hit: %s', body.get('event').get('reaction'))
 
         headers = {
             'Content-Type': 'application/json; charset=UTF-8',
             'Authorization': 'Bearer {0}'.format(BOT_TOKEN)
         }
-        # reaction定義
-        emoji = ADD_REACTIONS.split(',')
-        for e in emoji:
-            print(e)
-            data = {
-                'token': OAUTH_TOKEN,
-                'channel': body.get('event').get('item').get('channel'),
-                'timestamp': body.get('event').get('item').get('ts'),
-                'name': e
-            }
-            # POST処理
+
+        # added 1st reaction
+        if body.get('event').get('type') == 'reaction_added':
+            url = 'https://slack.com/api/reactions.add'
+            if body.get('event').get('reaction') == emojis[0] and body.get(
+                    'event').get('user', '') != 'U9KRXEKLG':
+                logger.info('human added 1st reaction')
+                print('set 1st emoji')
+                print(emojis[1])
+                data = {
+                    'token': OAUTH_TOKEN,
+                    'channel': body.get('event').get('item').get('channel'),
+                    'timestamp': body.get('event').get('item').get('ts'),
+                    'name': emojis[1]
+                }
+            if body.get('event').get('reaction') in emojis and body.get(
+                    'event').get('reaction') != emojis[0] and body.get(
+                        'event').get('user', '') == 'U9KRXEKLG':
+                logger.info('bot added 2st or later reaction')
+                last_count = len(emojis) - 1
+                this_count = emojis.index(body.get('event').get('reaction'))
+                if (this_count != last_count):
+                    logger.info('add: %s', emojis[this_count + 1])
+                    data = {
+                        'token': OAUTH_TOKEN,
+                        'channel':
+                        body.get('event').get('item').get('channel'),
+                        'timestamp': body.get('event').get('item').get('ts'),
+                        'name': emojis[this_count + 1]
+                    }
             req = urllib.request.Request(
                 url,
                 data=json.dumps(data).encode('utf-8'),
@@ -74,5 +90,25 @@ def handler(event, context):
                 headers=headers)
             res = urllib.request.urlopen(req)
             logger.info('post result: %s', res.msg)
+        elif body.get('event').get('type') == 'reaction_removed' and body.get(
+                'event').get('reaction') in emojis[0]:
+            logger.info('human removed 1st reaction')
+            url = 'https://slack.com/api/reactions.remove'
+            for e in emojis:
+                logger.info('remove: %s', e)
+                data = {
+                    'token': OAUTH_TOKEN,
+                    'channel': body.get('event').get('item').get('channel'),
+                    'timestamp': body.get('event').get('item').get('ts'),
+                    'name': e
+                }
+                # remove処理
+                req = urllib.request.Request(
+                    url,
+                    data=json.dumps(data).encode('utf-8'),
+                    method='POST',
+                    headers=headers)
+                res = urllib.request.urlopen(req)
+                logger.info('post result: %s', res.msg)
         return {'statusCode': 200, 'body': 'ok'}
     return {'statusCode': 200, 'body': 'quit'}
